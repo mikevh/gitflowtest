427ab33e864427e263daf5fabe4c1306dca9be41
Michael VanderHelm <mikevh@gmail.com>
2016-02-29 16:24:25 -0800

    new project

---
+++ src/Flow/Flow.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Flow.Web.Startup))]
+namespace Flow.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
